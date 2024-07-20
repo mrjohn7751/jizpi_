66e92a2456bd9d98ed29cf54b4949ec9558bd204
Samandar0912 <samandarnazarov07@gmail.com>
2024-07-21 00:46:03 +0500

    new-jizpi

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -13,6 +13,10 @@ urlpatterns = [
 
 )
 
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += [
+        re_path(r'^rosetta/', include('rosetta.urls'))
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
